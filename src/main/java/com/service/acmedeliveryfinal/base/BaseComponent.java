68b11508a7d5cd331e7a9ace3b3b2db3aaa947b2
g.prassas <4only4games@gmail.com>
2022-08-30 23:49:05 +0300

    Add necessary annotation and logger to class BaseModel

--- src/main/java/com/service/acmedeliveryfinal/base/BaseComponent.java
+++ src/main/java/com/service/acmedeliveryfinal/base/BaseComponent.java
@@ -1,4 +1,20 @@
 package com.service.acmedeliveryfinal.base;
 
-public class BaseComponent {
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+
+public abstract class BaseComponent {
+
+    protected Logger logger = LoggerFactory.getLogger(getClass());
+
+    @PostConstruct
+    public void init() {logger.trace("Loaded {} ", getClass());}
+
+    @PreDestroy
+    public void destroy(){logger.trace("{} is about to get Destroyed" , getClass().getName());}
+
+
 }
