974304c2e8d3f42a5420c64b025fc31b669a9d51
g.prassas <4only4games@gmail.com>
2022-08-30 23:32:19 +0300

    Add necessary annotation to class BaseModel

--- src/main/java/com/service/acmedeliveryfinal/domain/BaseModel.java
+++ src/main/java/com/service/acmedeliveryfinal/domain/BaseModel.java
@@ -1,4 +1,26 @@
 package com.service.acmedeliveryfinal.domain;
 
-public class BaseModel {
+import com.service.acmedeliveryfinal.base.BaseComponent;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serial;
+import java.io.Serializable;
+@Getter
+@Setter
+@ToString
+public abstract class BaseModel  implements Serializable {
+
+    @Serial
+    private static final Long serialVersionUID = 1L;
+    @Id
+    @Column(updatable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
 }
