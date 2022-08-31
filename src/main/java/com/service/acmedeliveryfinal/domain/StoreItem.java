d2161365ca72b2aae3a8cb5d32cc40475208960f
Giorgos <giorgosthf@gmail.com>
2022-08-31 22:05:51 +0300

    Store Item class create fields

--- src/main/java/com/service/acmedeliveryfinal/domain/StoreItem.java
+++ src/main/java/com/service/acmedeliveryfinal/domain/StoreItem.java
@@ -14,5 +14,18 @@ import java.math.BigDecimal;
 @Table(name = "STOREITEM")
 public class StoreItem extends BaseModel {
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @ToString.Exclude
+    private Product product;
 
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @ToString.Exclude
+    private Store store;
+
+    @Column(nullable = false)
+    private Integer stock;
+
+    @Column(precision = 10, scale = 2, nullable = false)
+    private BigDecimal price;
 }
