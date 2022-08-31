9367d47fb549a80e68bad48bff702410e3b01d3d
Giorgos <giorgosthf@gmail.com>
2022-08-31 21:54:44 +0300

    Add Store Class

---
+++ src/main/java/com/service/acmedeliveryfinal/domain/Store.java
@@ -0,0 +1,26 @@
+package com.service.acmedeliveryfinal.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.Set;
+
+
+@Entity
+@Table(name = "store")
+@Getter
+@Setter
+@Builder
+@ToString(callSuper = true)
+@NoArgsConstructor
+@AllArgsConstructor
+public class Store extends BaseModel {
+
+
+
+
+
+
+
+}
