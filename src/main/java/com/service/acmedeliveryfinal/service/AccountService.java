d47b481a9a435869d38b76408a058ca8b2189d60
g.chatzidakis <g.chatzidakis@pmmretail.com>
2022-08-31 22:57:39 +0300

    fill AccountService with some method declarations

--- src/main/java/com/service/acmedeliveryfinal/service/AccountService.java
+++ src/main/java/com/service/acmedeliveryfinal/service/AccountService.java
@@ -1,7 +1,20 @@
 package com.service.acmedeliveryfinal.service;
 
 import com.service.acmedeliveryfinal.domain.Account;
+import com.service.acmedeliveryfinal.domain.Address;
+
+import java.util.List;
+import java.util.Set;
 
 public interface AccountService extends BaseService<Account>{
 
+    //Aggregate methods
+
+    //List<Address> findAllAddresses();
+    //Address findAddress(Address address);
+     void addAddress(Account account, Address address);
+    //Address removeAddress(Address address);
+    //Address updateAddress(Address address);
+
+    //
 }
