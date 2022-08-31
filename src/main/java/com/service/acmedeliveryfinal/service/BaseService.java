c924c1eb41f602feb93b529a3694f3874d2b8de9
g.chatzidakis <g.chatzidakis@pmmretail.com>
2022-08-31 17:07:26 +0300

    Fill in BaseService

--- src/main/java/com/service/acmedeliveryfinal/service/BaseService.java
+++ src/main/java/com/service/acmedeliveryfinal/service/BaseService.java
@@ -2,11 +2,28 @@ package com.service.acmedeliveryfinal.service;
 
 import com.service.acmedeliveryfinal.domain.BaseModel;
 
-
+import java.util.List;
 
 
 public interface BaseService<T extends BaseModel> {
 
+    T create(final T entity);
+
+    List<T> createAll(final T... entities);
+
+    List<T> createAll(final List<T> entities);
+
+    void update(T entity);
+
+    void delete(T entity);
+
+    void deleteById(Long id);
+
+    boolean exists(T entity);
+
+    T get(Long id);
+
+    List<T> findAll();
 
 
 }
