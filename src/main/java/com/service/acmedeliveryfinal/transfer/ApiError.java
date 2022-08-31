a900f493107b16ec0e5b58e232850fe4ab76ef5e
g.prassas <4only4games@gmail.com>
2022-09-01 02:51:41 +0300

    Add helper classes

---
+++ src/main/java/com/service/acmedeliveryfinal/transfer/ApiError.java
@@ -0,0 +1,14 @@
+package com.service.acmedeliveryfinal.transfer;
+
+import lombok.Builder;
+import lombok.Value;
+
+import java.io.Serializable;
+
+@Value
+@Builder
+public class ApiError implements Serializable {
+	String description;
+	Integer httpStatus;
+	String path;
+}
