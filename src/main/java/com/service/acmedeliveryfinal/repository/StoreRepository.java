4abf0414f3667e98792a611cb3697525614b8e2b
g.prassas <4only4games@gmail.com>
2022-09-01 03:13:42 +0300

    implement store repository methods

--- src/main/java/com/service/acmedeliveryfinal/repository/StoreRepository.java
+++ src/main/java/com/service/acmedeliveryfinal/repository/StoreRepository.java
@@ -1,7 +1,15 @@
 package com.service.acmedeliveryfinal.repository;
 
 import com.service.acmedeliveryfinal.domain.Store;
+import com.service.acmedeliveryfinal.domain.enumeration.StoreCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface StoreRepository extends JpaRepository<Store,Long> {
+    @Query(value = "SELECT s FROM Store s WHERE s.storeCategory = ?1")
+    List<Store> getStoresByCategory(String category);
+    @Query(value = "SELECT s FROM Store s WHERE s.storeName = ?1")
+    List<Store> getStoresByName(String name);
 }
