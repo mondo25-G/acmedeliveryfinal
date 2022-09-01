e55ad1cf679e0b65d81540fd18abb68df2c88434
g.prassas <4only4games@gmail.com>
2022-09-01 03:13:13 +0300

    implement store service get top products

--- src/main/java/com/service/acmedeliveryfinal/service/StoreServiceImpl.java
+++ src/main/java/com/service/acmedeliveryfinal/service/StoreServiceImpl.java
@@ -1,10 +1,16 @@
 package com.service.acmedeliveryfinal.service;
 
+import com.service.acmedeliveryfinal.domain.Product;
 import com.service.acmedeliveryfinal.domain.Store;
+import com.service.acmedeliveryfinal.domain.enumeration.StoreCategory;
 import com.service.acmedeliveryfinal.repository.StoreRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -13,4 +19,15 @@ public class StoreServiceImpl extends BaseServiceImpl<Store> implements StoreSer
     private final StoreRepository storeRepository;
     @Override
     public JpaRepository<Store, Long> getRepository() {return storeRepository;}
+
+
+    public List<Store> getStoresByName(String name){
+        List<Store> storesByName = storeRepository.getStoresByName(name);
+        return storesByName;
+    }
+
+    public List<Store> getStoresByCategory(StoreCategory category){
+        List<Store> storesByName = storeRepository.getStoresByCategory(category.toString());
+        return storesByName;
+    }
 }
