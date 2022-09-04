8c220ab7ada971ea46c0ef0bf32aba295bf84e49
Giorgos <giorgosthf@gmail.com>
2022-09-04 20:18:10 +0300

    Create Get Menu by Store Method

--- src/main/java/com/service/acmedeliveryfinal/service/StoreItemService.java
+++ src/main/java/com/service/acmedeliveryfinal/service/StoreItemService.java
@@ -1,10 +1,14 @@
 package com.service.acmedeliveryfinal.service;
 
+import com.service.acmedeliveryfinal.domain.Store;
 import com.service.acmedeliveryfinal.domain.StoreItem;
 
 import java.util.List;
 
 public interface StoreItemService extends BaseService<StoreItem> {
+
+
+    List<StoreItem> getMenu(Store store);    //By The Order of Mister G.Prassas I Present you the new Method
     StoreItem findBySerial(String serial);
     List<StoreItem> topProductsByRanking(Integer rankingThreshold);
 }
