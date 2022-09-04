f63bb28baaf09ce03b8cdaf2db08e09ab2370419
Giorgos <giorgosthf@gmail.com>
2022-09-04 19:29:27 +0300

    Adjustments to fields and extension

--- src/main/java/com/service/acmedeliveryfinal/domain/Store.java
+++ src/main/java/com/service/acmedeliveryfinal/domain/Store.java
@@ -16,29 +16,19 @@ import java.util.Set;
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "STORES")
-public class Store extends BaseModel {
-
+public class Store extends BaseEntity {
 
 
     @Column(nullable = false)
     String storeName;
 
-    @Column
-    String businessName;
 
-    @Column
-    private String phoneNumber;
-
-    @Column
-    private Boolean isActive;
-
-//
-//    @ToString.Exclude
-//    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, mappedBy="store")
-//    Set<StoreItem> menuItems;
+    @ToString.Exclude
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, mappedBy="store")
+    Set<StoreItem> menuItems;
 
 
-    @Column
-    private String storeCategory;
+    @Enumerated(EnumType.STRING)
+    StoreCategory storeCategory;
 
 }
