048a6e47ef4ac7149637a774a8c11755e5d0ab28
Giorgos <giorgosthf@gmail.com>
2022-09-04 19:36:24 +0300

    Create Store Methods

--- src/main/java/com/service/acmedeliveryfinal/service/StoreService.java
+++ src/main/java/com/service/acmedeliveryfinal/service/StoreService.java
@@ -1,6 +1,12 @@
 package com.service.acmedeliveryfinal.service;
 
 import com.service.acmedeliveryfinal.domain.Store;
+import com.service.acmedeliveryfinal.domain.enumeration.StoreCategory;
+
+import java.util.List;
 
 public interface StoreService extends BaseService<Store>{
+    List<Store> getStoresByName(String name);
+
+    List<Store> getStoresByCategory(StoreCategory storeCategoryEnum);
 }
