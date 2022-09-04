40315e658257c95e3d4141c314fdf51a4195c990
Giorgos <giorgosthf@gmail.com>
2022-09-04 19:12:15 +0300

    Refactor extension from BaseModel to BaseEntity

--- src/main/java/com/service/acmedeliveryfinal/controller/BaseController.java
+++ src/main/java/com/service/acmedeliveryfinal/controller/BaseController.java
@@ -1,12 +1,12 @@
 package com.service.acmedeliveryfinal.controller;
 
 import com.service.acmedeliveryfinal.base.BaseComponent;
-import com.service.acmedeliveryfinal.domain.BaseModel;
+import com.service.acmedeliveryfinal.domain.BaseEntity;
 import com.service.acmedeliveryfinal.service.BaseService;
 
 
 
-public abstract class BaseController<T extends BaseModel> extends BaseComponent {
+public abstract class BaseController<T extends BaseEntity> extends BaseComponent {
 
     protected abstract BaseService<T> getBaseService();
 
