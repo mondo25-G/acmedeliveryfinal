47d2a8f0e6d7b6ceed2284cb1d2130e7333683bd
g.chatzidakis <g.chatzidakis@pmmretail.com>
2022-09-05 17:09:56 +0300

    remove everything Address related from AccountService

--- src/main/java/com/service/acmedeliveryfinal/service/AccountService.java
+++ src/main/java/com/service/acmedeliveryfinal/service/AccountService.java
@@ -8,13 +8,5 @@ import java.util.Set;
 
 public interface AccountService extends BaseService<Account>{
 
-    //Aggregate methods
 
-    //List<Address> findAllAddresses();
-    //Address findAddress(Address address);
-     void addAddress(Account account, Address address);
-    //Address removeAddress(Address address);
-    //Address updateAddress(Address address);
-
-    //
 }
