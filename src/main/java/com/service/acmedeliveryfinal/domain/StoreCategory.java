99fddba55de4f20f2de1d4b1cd5d68a3732ddd2e
g.chatzidakis <g.chatzidakis@pmmretail.com>
2022-09-08 19:21:04 +0300

    Add StoreCategory entity

---
+++ src/main/java/com/service/acmedeliveryfinal/domain/StoreCategory.java
@@ -0,0 +1,21 @@
+package com.service.acmedeliveryfinal.domain;
+
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "STORE_CATEGORY")
+@Getter
+@Setter
+@Builder
+@ToString(callSuper = true)
+@NoArgsConstructor
+@AllArgsConstructor
+public class StoreCategory extends BaseEntity{
+
+    @Column(unique = true)
+    private String name;
+}
