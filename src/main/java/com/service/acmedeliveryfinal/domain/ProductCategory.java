1a458d5529f277a456dd72d49121415404a834f8
g.chatzidakis <g.chatzidakis@pmmretail.com>
2022-09-08 19:17:32 +0300

    Add ProductCategory entity

---
+++ src/main/java/com/service/acmedeliveryfinal/domain/ProductCategory.java
@@ -0,0 +1,23 @@
+package com.service.acmedeliveryfinal.domain;
+
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+@Entity
+@Table(name = "PRODUCT_CATEGORY")
+@Getter
+@Setter
+@Builder
+@ToString(callSuper = true)
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductCategory extends BaseEntity {
+
+
+    @Column(unique = true)
+    private String name;
+
+
+}
