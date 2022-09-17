854a6046e197783729177092410f7503a686d28a
g.prassas <4only4games@gmail.com>
2022-09-17 19:29:04 +0300

    remove unnecessary imports

--- src/main/java/com/service/acmedeliveryfinal/controller/StoreItemController.java
+++ src/main/java/com/service/acmedeliveryfinal/controller/StoreItemController.java
@@ -1,17 +1,9 @@
 package com.service.acmedeliveryfinal.controller;
 
-
-import com.service.acmedeliveryfinal.domain.StoreItem;
 import com.service.acmedeliveryfinal.service.StoreItemService;
-import com.service.acmedeliveryfinal.transfer.ApiResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,13 +12,4 @@ public class StoreItemController {
 
     private final StoreItemService storeItemService;
 
-    //CRUD
-
-    @GetMapping("/getTopProductsByRanking")
-    public ResponseEntity<ApiResponse<List<StoreItem>>> getTopProductsByRanking(@RequestParam Integer rankingThreshold) {
-
-        final List<StoreItem> topProductsByRanking = storeItemService.topProductsByRanking(rankingThreshold);
-        return ResponseEntity.ok(ApiResponse.<List<StoreItem>>builder().data(topProductsByRanking).build());
-
-    }
 }
--- src/main/java/com/service/acmedeliveryfinal/service/StoreItemServiceImpl.java
+++ src/main/java/com/service/acmedeliveryfinal/service/StoreItemServiceImpl.java
@@ -6,8 +6,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
 @Service
 @RequiredArgsConstructor
 public class StoreItemServiceImpl extends BaseServiceImpl<StoreItem> implements StoreItemService {
