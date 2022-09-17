7656467adaee0df6c56552171cf5669e659bb412
Giorgos <giorgosthf@gmail.com>
2022-09-17 18:59:41 +0300

    Refactor StoreService

--- src/main/java/com/service/acmedeliveryfinal/service/StoreService.java
+++ src/main/java/com/service/acmedeliveryfinal/service/StoreService.java
@@ -1,14 +1,12 @@
 package com.service.acmedeliveryfinal.service;
 
 import com.service.acmedeliveryfinal.domain.Store;
-import com.service.acmedeliveryfinal.domain.enumeration.StoreCategory;
 
 import java.util.List;
 
+
 public interface StoreService extends BaseService<Store>{
-    List<Store> getStoresByName(String name);
+    Store getLazy(Long id);
 
-    List<Store> getStoresByCategory(StoreCategory storeCategoryEnum);
-
-    Store getStore(Long id);
+    List<Store> getLazyAll();
 }
