eac37f837ce03817d4e4f9e70099eca07c5685e3
g.chatzidakis <g.chatzidakis@pmmretail.com>
2022-09-17 19:14:23 +0300

    Rename ProductCategory, StoreCategory tableNames to plural

--- src/main/java/com/service/acmedeliveryfinal/domain/ProductCategory.java
+++ src/main/java/com/service/acmedeliveryfinal/domain/ProductCategory.java
@@ -6,7 +6,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 @Entity
-@Table(name = "PRODUCT_CATEGORY")
+@Table(name = "PRODUCT_CATEGORIES")
 @Getter
 @Setter
 @Builder
--- src/main/java/com/service/acmedeliveryfinal/domain/StoreCategory.java
+++ src/main/java/com/service/acmedeliveryfinal/domain/StoreCategory.java
@@ -7,7 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "STORE_CATEGORY")
+@Table(name = "STORE_CATEGORIES")
 @Getter
 @Setter
 @Builder
