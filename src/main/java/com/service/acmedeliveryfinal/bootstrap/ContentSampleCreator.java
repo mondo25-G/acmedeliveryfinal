5c0e4a33e8764d00e1242fba362ebdf2f2fdfb28
g.chatzidakis <g.chatzidakis@pmmretail.com>
2022-09-17 19:20:20 +0300

    Remove obsolete seeding from ContentSampleCreator

--- src/main/java/com/service/acmedeliveryfinal/bootstrap/ContentSampleCreator.java
+++ src/main/java/com/service/acmedeliveryfinal/bootstrap/ContentSampleCreator.java
@@ -1,10 +1,6 @@
 package com.service.acmedeliveryfinal.bootstrap;
 
 import com.service.acmedeliveryfinal.base.BaseComponent;
-import com.service.acmedeliveryfinal.domain.Account;
-import com.service.acmedeliveryfinal.domain.Store;
-import com.service.acmedeliveryfinal.domain.enumeration.StoreCategory;
-import com.service.acmedeliveryfinal.service.AccountService;
 import com.service.acmedeliveryfinal.service.StoreService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
@@ -21,15 +17,6 @@ public class ContentSampleCreator extends BaseComponent implements CommandLineRu
 
     @Override
     public void run(String... args) throws Exception {
-        storeService.create(Store.builder().storeName("Store A").storeCategory(StoreCategory.BURGERS).build());
-        storeService.create(Store.builder().storeName("Store B").storeCategory(StoreCategory.BURGERS).build());
-        storeService.create(Store.builder().storeName("Store C").storeCategory(StoreCategory.PIZZERIA).build());
-        storeService.create(Store.builder().storeName("Store D").storeCategory(StoreCategory.PIZZERIA).build());
-        storeService.create(Store.builder().storeName("Store E").storeCategory(StoreCategory.PIZZERIA).build());
-        storeService.create(Store.builder().storeName("Store F").storeCategory(StoreCategory.RESTAURANT).build());
-        storeService.create(Store.builder().storeName("Store G").storeCategory(StoreCategory.RESTAURANT).build());
-        storeService.create(Store.builder().storeName("Store H").storeCategory(StoreCategory.RESTAURANT).build());
-        storeService.create(Store.builder().storeName("Store I").storeCategory(StoreCategory.BURGERS).build());
 
     }
     //@formatter: on
