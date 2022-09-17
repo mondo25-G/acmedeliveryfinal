341ef827dd27e649d505da0378719106a9c4ea97
g.chatzidakis <g.chatzidakis@pmmretail.com>
2022-09-17 19:08:12 +0300

    Add methods getLazy, getLazyAll to StoreRepository

--- src/main/java/com/service/acmedeliveryfinal/repository/StoreRepository.java
+++ src/main/java/com/service/acmedeliveryfinal/repository/StoreRepository.java
@@ -4,12 +4,19 @@ import com.service.acmedeliveryfinal.domain.Store;
 import com.service.acmedeliveryfinal.domain.enumeration.StoreCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.QueryHints;
 
+import javax.persistence.QueryHint;
 import java.util.List;
+import java.util.Optional;
 
 public interface StoreRepository extends JpaRepository<Store,Long> {
-    @Query(value = "SELECT s FROM Store s WHERE s.storeCategory = ?1")
-    List<Store> getStoresByCategory(String category);
-    @Query(value = "SELECT s FROM Store s WHERE s.storeName = ?1")
-    List<Store> getStoresByName(String name);
+
+    @Query("select distinct s from Store s join fetch s.storeItems si join fetch s.category join fetch si.productCategory where s.id = :id")
+    @QueryHints(@QueryHint(name = org.hibernate.jpa.QueryHints.HINT_PASS_DISTINCT_THROUGH, value = "false"))
+    Optional<Store> getLazy(Long id);
+
+    @Query("select distinct s from Store s join fetch s.storeItems si join fetch s.category join fetch si.productCategory")
+    @QueryHints(@QueryHint(name = org.hibernate.jpa.QueryHints.HINT_PASS_DISTINCT_THROUGH, value = "false"))
+    List<Store> getLazyAll();
 }
