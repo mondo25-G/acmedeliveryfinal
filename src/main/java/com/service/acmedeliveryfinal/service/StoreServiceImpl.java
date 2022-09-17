78ba7bf244f93fc48024f7a50afe660d0704d8b6
g.chatzidakis <g.chatzidakis@pmmretail.com>
2022-09-17 20:44:25 +0300

    Implement StoreItem crud methods in StoreServiceImpl

--- src/main/java/com/service/acmedeliveryfinal/service/StoreServiceImpl.java
+++ src/main/java/com/service/acmedeliveryfinal/service/StoreServiceImpl.java
@@ -2,6 +2,7 @@ package com.service.acmedeliveryfinal.service;
 
 
 import com.service.acmedeliveryfinal.domain.Store;
+import com.service.acmedeliveryfinal.domain.StoreItem;
 import com.service.acmedeliveryfinal.repository.StoreRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -34,5 +36,46 @@ public class StoreServiceImpl extends BaseServiceImpl<Store> implements StoreSer
     @Override
     public List<Store> getLazyAll() {
         return storeRepository.getLazyAll();
+    }
+
+    @Override
+    public void addItems(Store store, List<StoreItem> items) {
+        for (StoreItem item : items)
+        {
+            store.getStoreItems().add(item);
+            item.setStore(store);
+        }
+        storeRepository.save(store);
+    }
+
+    @Override
+    public void addItem(Long id, StoreItem item) {
+        Store store =storeRepository.findById(id).get();
+        logger.info("Update Store: {} ",store );
+        logger.info("With Item: {}",item);
+        store.getStoreItems().add(item);
+        item.setStore(store);
+        storeRepository.save(store);
+    }
+
+    @Override
+    public void removeItem(Long id, Long sid) {
+        Store store =storeRepository.findById(id).get();
+        store.getStoreItems().removeIf(storeItem -> Objects.equals(storeItem.getId(), sid));
+        logger.info("Delete Item: {} ",store);
+        storeRepository.save(store);
+    }
+
+    @Override
+    public void updateItem(Long id, StoreItem item) {
+        Store store =storeRepository.findById(id).get();
+        logger.info("Update Store: {} ",store);
+        logger.info("Update Item: {} ",item);
+
+        store.getStoreItems().removeIf
+                (storeItem -> Objects.equals(storeItem.getId(),item.getId()));
+        store.getStoreItems().add(item);
+
+        storeRepository.save(store);
     }
 }
