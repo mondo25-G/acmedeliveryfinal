a40110e988272c04d1999f9251c91a951143daa9
g.chatzidakis <g.chatzidakis@pmmretail.com>
2022-09-22 19:36:06 +0300

    Declare native wuery to StoreItemRepository for top ten products

--- src/main/java/com/service/acmedeliveryfinal/repository/StoreItemRepository.java
+++ src/main/java/com/service/acmedeliveryfinal/repository/StoreItemRepository.java
@@ -1,10 +1,17 @@
 package com.service.acmedeliveryfinal.repository;
 
 import com.service.acmedeliveryfinal.domain.StoreItem;
+import com.service.acmedeliveryfinal.transfer.KeyValue;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 
 @Repository
 public interface StoreItemRepository extends JpaRepository<StoreItem , Long> {
+
+    @Query(name="StoreItem.findTop10Products",nativeQuery = true)
+    List<KeyValue<Long,String>> top10StoreItemsBasedOnOrderLines();
 }
