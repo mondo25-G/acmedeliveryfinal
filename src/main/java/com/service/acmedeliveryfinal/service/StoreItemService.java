653b2220f9f91e817629996395b3aabd938f86ff
g.chatzidakis <g.chatzidakis@pmmretail.com>
2022-09-23 20:15:57 +0300

    RemoveStoreItemService

--- src/main/java/com/service/acmedeliveryfinal/service/StoreItemService.java
+++
@@ -1,9 +0,0 @@
-package com.service.acmedeliveryfinal.service;
-
-import com.service.acmedeliveryfinal.domain.StoreItem;
-
-import java.util.List;
-
-public interface StoreItemService extends BaseService<StoreItem> {
-
-}
