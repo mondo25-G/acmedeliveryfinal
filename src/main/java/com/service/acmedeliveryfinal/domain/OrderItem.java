c4d963d7c830f5e67c659f042c74c431e965fc23
g.chatzidakis <g.chatzidakis@pmmretail.com>
2022-09-24 01:02:49 +0300

    Add NotNull annotations to OrderItem

--- src/main/java/com/service/acmedeliveryfinal/domain/OrderItem.java
+++ src/main/java/com/service/acmedeliveryfinal/domain/OrderItem.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 
 @Getter
@@ -15,19 +16,24 @@ import java.math.BigDecimal;
 @Entity
 @Table(name = "ORDERITEMS")
 public class OrderItem extends BaseEntity{
+
+    @NotNull
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "storeItem_id")
     private StoreItem storeItem;
 
+    @NotNull
     @ToString.Exclude
     @ManyToOne( fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "order_id")
     @JsonIgnore
     private Order order;
 
+    @NotNull
     @Column(nullable = false)
     private Integer quantity;
 
+    @NotNull
     @Column(precision = 10, scale = 2,nullable = false)
     private BigDecimal price;
 
