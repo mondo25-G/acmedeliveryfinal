0ff3737ae33e5efc6e1c815531203e779067e14e
g.chatzidakis <g.chatzidakis@pmmretail.com>
2022-09-23 21:05:43 +0300

    Refactor OrderContentCreator testing just add product methods to have samples to test top product/ top store queries

--- src/main/java/com/service/acmedeliveryfinal/bootstrap/OrderContentCreator.java
+++ src/main/java/com/service/acmedeliveryfinal/bootstrap/OrderContentCreator.java
@@ -1,15 +1,18 @@
 package com.service.acmedeliveryfinal.bootstrap;
 
 import com.service.acmedeliveryfinal.base.BaseComponent;;
+import com.service.acmedeliveryfinal.domain.Store;
+import com.service.acmedeliveryfinal.domain.StoreItem;
 import com.service.acmedeliveryfinal.domain.enumeration.PaymentMethod;
 import com.service.acmedeliveryfinal.service.AccountService;
 import com.service.acmedeliveryfinal.service.OrderService;
-import com.service.acmedeliveryfinal.service.StoreItemService;
 import com.service.acmedeliveryfinal.service.StoreService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 
 @Component
@@ -19,7 +22,6 @@ public class OrderContentCreator extends BaseComponent implements CommandLineRun
 
     private final OrderService orderService;
     private final StoreService storeService;
-    private final StoreItemService storeItemService;
     private final AccountService accountService;
 
     String addItem = "Add Item : {} ";
@@ -29,112 +31,79 @@ public class OrderContentCreator extends BaseComponent implements CommandLineRun
 
 
     @Override
-    public void run(String... args)  {
+    public void run(String... args) throws InterruptedException {
 
         // ORDER 1 - INITIALIZE WITH STORE AND ACCOUNT
         com.service.acmedeliveryfinal.domain.Order newOrder =
                 orderService.initiateOrder(storeService.get(1L), accountService.get(1L));
+        Store store1 = storeService.getLazy(1L);
+        List<StoreItem> store1Items = store1.getStoreItems().stream().toList();
 
-        // ORDER 1 - ADD ITEMS AND QUANTITY , TEST NON EXISTING ITEMS IN STORE
-        logger.info(addItem, orderService.addItem(newOrder, storeItemService.get(1L), 2));
-        logger.info(addItem, orderService.addItem(newOrder, storeItemService.get(2L), 2));
-        logger.info(addItem, orderService.addItem(newOrder, storeItemService.get(3L), 2));
-        logger.info(addItem, orderService.addItem(newOrder, storeItemService.get(4L), 2));
-        logger.info(addItem, orderService.addItem(newOrder, storeItemService.get(10L), 2));
-        logger.info(addItem, orderService.addItem(newOrder, storeItemService.get(4L), 2));
-        logger.info(addItem, orderService.addItem(newOrder, storeItemService.get(9L), 2));
 
-        // ORDER 1 -REMOVE AND UPDATE ITEMS WITH NEW QUANTITY
-        logger.info(removeItem, orderService.removeItem(newOrder, storeItemService.get(4L)));
-        logger.info(updateItem, orderService.updateItem(newOrder, storeItemService.get(2L), 3));
+        // ORDER 1 - ADD ITEMS AND QUANTITY
+        logger.info(addItem, orderService.addItem(newOrder, storeService.getProduct(1L,1L), 2));
+        logger.info(addItem, orderService.addItem(newOrder, storeService.getProduct(1L,2L), 3));
+        logger.info(addItem, orderService.addItem(newOrder, storeService.getProduct(1L,3L), 1));
+        logger.info(addItem, orderService.addItem(newOrder, storeService.getProduct(1L,4L), 1));
 
         // ORDER 1 - CHECKOUT OF ORDER
         orderService.checkout(newOrder, PaymentMethod.CASH);
         logger.info(orderComplete, newOrder);
+
+        //ok
 
 
         // ORDER 2 - INITIALIZE WITH STORE AND ACCOUNT
         com.service.acmedeliveryfinal.domain.Order newOrder2 =
                 orderService.initiateOrder(storeService.get(2L),accountService.get(2L));
 
-        // ORDER 2 - ADD ITEMS AND QUANTITY , TEST NON EXISTING ITEM IN STORE
-        logger.info(addItem,orderService.addItem(newOrder2,storeItemService.get(1L),2));   // Not In Store
-        logger.info(addItem,orderService.addItem(newOrder2,storeItemService.get(8L),2));   //Not In Store
-        logger.info(addItem,orderService.addItem(newOrder2,storeItemService.get(10L),2));  //In Store
-        logger.info(addItem,orderService.addItem(newOrder2,storeItemService.get(11L),2));  // In Store
-        logger.info(addItem,orderService.addItem(newOrder2,storeItemService.get(4L),2));   // Not In Store
-        logger.info(addItem,orderService.addItem(newOrder2,storeItemService.get(9L),2));   // In Store
-        logger.info(addItem,orderService.addItem(newOrder2,storeItemService.get(6L),2));   // Not In Store
-
-        // ORDER 2 - REMOVE AND UPDATE ITEMS WITH NEW QUANTITY
-        logger.info(removeItem,orderService.removeItem(newOrder2,storeItemService.get(9L)));
-        logger.info(updateItem,orderService.updateItem(newOrder2,storeItemService.get(10L),3));
+        // ORDER 2 - ADD ITEMS AND QUANTITY
+        logger.info(addItem,orderService.addItem(newOrder2,storeService.getProduct(10L),2));
+        logger.info(addItem,orderService.addItem(newOrder2,storeService.getProduct(11L),2));
+        logger.info(addItem,orderService.addItem(newOrder2,storeService.getProduct(9L),2));
 
         // ORDER 2 - CHECKOUT OF ORDER
         orderService.checkout(newOrder2, PaymentMethod.CARD);
         logger.info(orderComplete,newOrder2);
 
-
         // ORDER 3 - INITIALIZE WITH STORE AND ACCOUNT
         com.service.acmedeliveryfinal.domain.Order newOrder3 =
-                orderService.initiateOrder(storeService.get(3L),accountService.get(3L));
+                orderService.initiateOrder(storeService.get(2L),accountService.get(3L));
 
-        // ORDER 3 - ADD ITEMS AND QUANTITY , TEST NON EXISTING ITEM IN STORE
-        logger.info(addItem,orderService.addItem(newOrder3,storeItemService.get(15L),2));   // Not In Store
-        logger.info(addItem,orderService.addItem(newOrder3,storeItemService.get(14L),2));   //Not In Store
-        logger.info(addItem,orderService.addItem(newOrder3,storeItemService.get(17L),2));  //In Store
-        logger.info(addItem,orderService.addItem(newOrder3,storeItemService.get(16L),2));  // In Store
-        logger.info(addItem,orderService.addItem(newOrder3,storeItemService.get(10L),2));   // Not In Store
-        logger.info(addItem,orderService.addItem(newOrder3,storeItemService.get(18L),2));   // In Store
-        logger.info(addItem,orderService.addItem(newOrder3,storeItemService.get(9L),2));   // Not In Store
-
-        // ORDER 3 - REMOVE AND UPDATE ITEMS WITH NEW QUANTITY
-        logger.info(removeItem,orderService.removeItem(newOrder3,storeItemService.get(17L)));
-        logger.info(updateItem,orderService.updateItem(newOrder3,storeItemService.get(18L),3));
+        // ORDER 3 - ADD ITEMS AND QUANTITY
+        logger.info(addItem,orderService.addItem(newOrder3,storeService.getProduct(13L),3));  //In Store
+        logger.info(addItem,orderService.addItem(newOrder3,storeService.getProduct(11L),2));  // In Store
+        logger.info(addItem,orderService.addItem(newOrder3,storeService.getProduct(15L),4));   // In Store
 
         // ORDER 3 - CHECKOUT OF ORDER
         orderService.checkout(newOrder3, PaymentMethod.CARD);
         logger.info(orderComplete,newOrder3);
 
 
+
         // ORDER 4 - INITIALIZE WITH STORE AND ACCOUNT
         com.service.acmedeliveryfinal.domain.Order newOrder4 =
-                orderService.initiateOrder(storeService.get(1L), accountService.get(1L));
+                orderService.initiateOrder(storeService.get(3L), accountService.get(3L));
 
-        // ORDER 4 - ADD ITEMS AND QUANTITY , TEST NON EXISTING ITEMS IN STORE
-        logger.info(addItem, orderService.addItem(newOrder4, storeItemService.get(1L), 2));
-        logger.info(addItem, orderService.addItem(newOrder4, storeItemService.get(2L), 2));
-        logger.info(addItem, orderService.addItem(newOrder4, storeItemService.get(3L), 2));
-        logger.info(addItem, orderService.addItem(newOrder4, storeItemService.get(4L), 2));
-        logger.info(addItem, orderService.addItem(newOrder4, storeItemService.get(10L), 2));
-        logger.info(addItem, orderService.addItem(newOrder4, storeItemService.get(4L), 2));
-        logger.info(addItem, orderService.addItem(newOrder4, storeItemService.get(9L), 2));
-
-        // ORDER 4 -REMOVE AND UPDATE ITEMS WITH NEW QUANTITY
-        logger.info(removeItem, orderService.removeItem(newOrder4, storeItemService.get(4L)));
-        logger.info(updateItem, orderService.updateItem(newOrder4, storeItemService.get(2L), 3));
+        // ORDER 4 - ADD ITEMS AND QUANTITY
+        logger.info(addItem, orderService.addItem(newOrder4, storeService.getProduct(19L), 2));
+        logger.info(addItem, orderService.addItem(newOrder4, storeService.getProduct(17L), 1));
+        logger.info(addItem, orderService.addItem(newOrder4, storeService.getProduct(18L), 2));
 
         // ORDER 4 - CHECKOUT OF ORDER
-        orderService.checkout(newOrder, PaymentMethod.CASH);
-        logger.info(orderComplete, newOrder);
+        orderService.checkout(newOrder4, PaymentMethod.CARD);
+        logger.info(orderComplete, newOrder4);
 
 
         // ORDER 5 - INITIALIZE WITH STORE AND ACCOUNT
         com.service.acmedeliveryfinal.domain.Order newOrder5 =
-                orderService.initiateOrder(storeService.get(2L),accountService.get(2L));
+                orderService.initiateOrder(storeService.get(3L),accountService.get(2L));
 
         // ORDER 5 - ADD ITEMS AND QUANTITY , TEST NON EXISTING ITEM IN STORE
-        logger.info(addItem,orderService.addItem(newOrder5,storeItemService.get(1L),2));   // Not In Store
-        logger.info(addItem,orderService.addItem(newOrder5,storeItemService.get(8L),2));   //Not In Store
-        logger.info(addItem,orderService.addItem(newOrder5,storeItemService.get(10L),2));  //In Store
-        logger.info(addItem,orderService.addItem(newOrder5,storeItemService.get(11L),2));  // In Store
-        logger.info(addItem,orderService.addItem(newOrder5,storeItemService.get(4L),2));   // Not In Store
-        logger.info(addItem,orderService.addItem(newOrder5,storeItemService.get(9L),2));   // In Store
-        logger.info(addItem,orderService.addItem(newOrder5,storeItemService.get(6L),2));   // Not In Store
+        logger.info(addItem,orderService.addItem(newOrder5,storeService.getProduct(22L),1));  //In Store
+        logger.info(addItem,orderService.addItem(newOrder5,storeService.getProduct(19L),1));  // In Store
+        logger.info(addItem,orderService.addItem(newOrder5,storeService.getProduct(18L),2));   // In Store
 
-        // ORDER 5 - REMOVE AND UPDATE ITEMS WITH NEW QUANTITY
-        logger.info(removeItem,orderService.removeItem(newOrder5,storeItemService.get(9L)));
-        logger.info(updateItem,orderService.updateItem(newOrder5,storeItemService.get(10L),3));
 
         // ORDER 5 - CHECKOUT OF ORDER
         orderService.checkout(newOrder5, PaymentMethod.CARD);
@@ -143,23 +112,19 @@ public class OrderContentCreator extends BaseComponent implements CommandLineRun
 
         // ORDER 6 - INITIALIZE WITH STORE AND ACCOUNT
         com.service.acmedeliveryfinal.domain.Order newOrder6 =
-                orderService.initiateOrder(storeService.get(3L),accountService.get(3L));
+                orderService.initiateOrder(storeService.get(4L),accountService.get(3L));
 
         // ORDER 6 - ADD ITEMS AND QUANTITY , TEST NON EXISTING ITEM IN STORE
-        logger.info(addItem,orderService.addItem(newOrder6,storeItemService.get(15L),2));   // Not In Store
-        logger.info(addItem,orderService.addItem(newOrder6,storeItemService.get(14L),2));   //Not In Store
-        logger.info(addItem,orderService.addItem(newOrder6,storeItemService.get(17L),2));  //In Store
-        logger.info(addItem,orderService.addItem(newOrder6,storeItemService.get(16L),2));  // In Store
-        logger.info(addItem,orderService.addItem(newOrder6,storeItemService.get(10L),2));   // Not In Store
-        logger.info(addItem,orderService.addItem(newOrder6,storeItemService.get(18L),2));   // In Store
-        logger.info(addItem,orderService.addItem(newOrder6,storeItemService.get(9L),2));   // Not In Store
-
-        // ORDER 6 - REMOVE AND UPDATE ITEMS WITH NEW QUANTITY
-        logger.info(removeItem,orderService.removeItem(newOrder6,storeItemService.get(17L)));
-        logger.info(updateItem,orderService.updateItem(newOrder6,storeItemService.get(18L),3));
+        logger.info(addItem,orderService.addItem(newOrder6,storeService.getProduct(23L),1));
+        logger.info(addItem,orderService.addItem(newOrder6,storeService.getProduct(24L),2));
+        logger.info(addItem,orderService.addItem(newOrder6,storeService.getProduct(25L),3));
+        logger.info(addItem,orderService.addItem(newOrder6,storeService.getProduct(26L),1));
 
         // ORDER 6 - CHECKOUT OF ORDER
         orderService.checkout(newOrder6, PaymentMethod.CARD);
         logger.info(orderComplete,newOrder6);
+
+
     }
+
 }
