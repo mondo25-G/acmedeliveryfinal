27b36ae8780170dbecdf32e7e5063d34eafaec98
g.chatzidakis <g.chatzidakis@pmmretail.com>
2022-09-23 20:55:44 +0300

    Add more queries to StoreRepository
    Add helper jpa queries for store seeding (may be eventually removed)
    Add native queries for top 10 products

--- src/main/java/com/service/acmedeliveryfinal/repository/StoreRepository.java
+++ src/main/java/com/service/acmedeliveryfinal/repository/StoreRepository.java
@@ -1,6 +1,7 @@
 package com.service.acmedeliveryfinal.repository;
 
 import com.service.acmedeliveryfinal.domain.Store;
+import com.service.acmedeliveryfinal.domain.StoreItem;
 import com.service.acmedeliveryfinal.transfer.KeyValue;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -28,6 +29,23 @@ public interface StoreRepository extends JpaRepository<Store,Long> {
     @QueryHints(@QueryHint(name = org.hibernate.jpa.QueryHints.HINT_PASS_DISTINCT_THROUGH, value = "false"))
     List<Store> findStoresByCategoryId(Long id);
 
+
+    //Jpa helper queries.
+
+    @Query("select distinct s from StoreItem s where s.store.id=:id")
+    @QueryHints(@QueryHint(name = org.hibernate.jpa.QueryHints.HINT_PASS_DISTINCT_THROUGH, value = "false"))
+    List<StoreItem> findStoreItemsByStoreId(Long id);
+
+    @Query("select distinct s from StoreItem s where s.store.id=:id and s.id=:sid")
+    @QueryHints(@QueryHint(name = org.hibernate.jpa.QueryHints.HINT_PASS_DISTINCT_THROUGH, value = "false"))
+    StoreItem findStoreItem(Long id,Long sid);
+
+    @Query("select distinct s from StoreItem s where s.id=:sid")
+    @QueryHints(@QueryHint(name = org.hibernate.jpa.QueryHints.HINT_PASS_DISTINCT_THROUGH, value = "false"))
+    StoreItem findStoreItem(Long sid);
+
+    //Native query methods to find popular stores. passed as DTOs.
+
    @Query(name="Store.findTop10Stores",nativeQuery = true)
     List<KeyValue<Long,String>> findTop10Stores();
 
@@ -40,4 +58,14 @@ public interface StoreRepository extends JpaRepository<Store,Long> {
 
     @Query(name="Store.findTop10StoresByCategoryName", nativeQuery = true)
     List<KeyValue<Long,String>> findTopStoresByCategory(String category);
+
+    //Native query methods to find popular store products. passed as DTOs.
+
+    //find all popular store products
+    @Query(name="StoreItem.findTop10Products",nativeQuery = true)
+    List<KeyValue<Long,String>> findTop10StoreItems();
+
+    //find all popular store products per store.
+    @Query(name="StoreItem.findTop10ProductsByStoreId",nativeQuery = true)
+    List<KeyValue<Long,String>> findTop10StoreItemsByStore(Long id);
 }
