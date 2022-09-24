e080f0909f349566ba47471767fb7874e9ab538a
g.chatzidakis <g.chatzidakis@pmmretail.com>
2022-09-24 03:47:12 +0300

    Add native queries for retrieving orders by account id order details by order id to OrderRepository

--- src/main/java/com/service/acmedeliveryfinal/repository/OrderRepository.java
+++ src/main/java/com/service/acmedeliveryfinal/repository/OrderRepository.java
@@ -1,6 +1,8 @@
 package com.service.acmedeliveryfinal.repository;
 
 import com.service.acmedeliveryfinal.domain.Order;
+import com.service.acmedeliveryfinal.transfer.AccountOrderHeaderDto;
+import com.service.acmedeliveryfinal.transfer.OrderDetailsDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.QueryHints;
@@ -22,5 +24,10 @@ public interface OrderRepository extends JpaRepository<Order, Long>  {
     @QueryHints(@QueryHint(name = org.hibernate.jpa.QueryHints.HINT_PASS_DISTINCT_THROUGH, value = "false"))
     List<Order> findByCustomer(Long id);
 
+
+    @Query(value = "select o.id as orderId,s.storeName as storeName,a.address as deliveryAddress,o.submittedDate as submittedDate,o.cost as cost from orders o inner join accounts a on a.id=o.account_id inner join stores s on o.store_id=s.id where a.id=?1",nativeQuery = true)
+    List<AccountOrderHeaderDto> getOrdersByAccount(Long id);
+    @Query(value = "select o.order_id as orderId,s.itemname as ItemName,o.price as price,o.quantity as quantity from orderItems o inner join storeitems s on s.id=o.storeitem_id where o.order_id=?1",nativeQuery = true)
+    List<OrderDetailsDto> getOrderItemsByOrder(Long id);
 
 }
