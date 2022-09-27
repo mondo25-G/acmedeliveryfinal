bc665bbe5418f2371700ffc6f6febd1acd50202c
g.chatzidakis <g.chatzidakis@pmmretail.com>
2022-09-27 18:48:15 +0300

    Add StoreItemDto

---
+++ src/main/java/com/service/acmedeliveryfinal/transfer/StoreItemDto.java
@@ -0,0 +1,13 @@
+package com.service.acmedeliveryfinal.transfer;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import java.math.BigDecimal;
+
+public interface StoreItemDto {
+    @JsonIgnore
+    Long getStoreId();
+    Long getId();
+    String getItemName();
+    BigDecimal getPrice();
+}
