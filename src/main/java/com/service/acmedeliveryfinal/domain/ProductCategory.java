dea27c40cf5d3fafe9f1af7dae6c2001345c69ae
g.chatzidakis <g.chatzidakis@pmmretail.com>
2022-09-28 20:55:15 +0300

    Add SequenceGenerator annotation in ProductCategory

--- src/main/java/com/service/acmedeliveryfinal/domain/ProductCategory.java
+++ src/main/java/com/service/acmedeliveryfinal/domain/ProductCategory.java
@@ -4,6 +4,7 @@ import lombok.*;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
@@ -15,6 +16,7 @@ import javax.validation.constraints.NotNull;
 @ToString(callSuper = true)
 @NoArgsConstructor
 @AllArgsConstructor
+@SequenceGenerator(name = "idGenerator", sequenceName = "PRODUCT_CATEGORIES_SEQ", initialValue = 1, allocationSize = 1)
 public class ProductCategory extends BaseEntity {
 
     @NotNull
