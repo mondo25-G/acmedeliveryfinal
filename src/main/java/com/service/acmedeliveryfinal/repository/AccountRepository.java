f00c3e7baa20c1185c77377db59caa4c00254a55
g.chatzidakis <g.chatzidakis@pmmretail.com>
2022-09-29 16:07:02 +0300

    Add findByEmailOrPassword, findByEmailAndPassword methods in AccountRepository

--- src/main/java/com/service/acmedeliveryfinal/repository/AccountRepository.java
+++ src/main/java/com/service/acmedeliveryfinal/repository/AccountRepository.java
@@ -1,20 +1,15 @@
 package com.service.acmedeliveryfinal.repository;
 
 import com.service.acmedeliveryfinal.domain.Account;
-import com.service.acmedeliveryfinal.domain.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.jpa.repository.QueryHints;
 import org.springframework.stereotype.Repository;
-import javax.persistence.QueryHint;
-import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
     Account findByEmail(String email);
-    @Query("select distinct o from Order o join fetch o.account where o.account.id = :id")
-    @QueryHints(@QueryHint(name = org.hibernate.jpa.QueryHints.HINT_PASS_DISTINCT_THROUGH, value = "false"))
-    Optional<List<Order>> getLazyOrders(Long id);
+    Optional<Account> findByEmailOrPhoneNumber(String email,String phoneNumber);
+
+    Optional<Account> findByEmailAndPassword(String email,String phoneNumber);
 
 }
