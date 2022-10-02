391246eab1f320342ea7cc386f6c4900506476d7
g.chatzidakis <g.chatzidakis@pmmretail.com>
2022-10-02 11:00:23 +0300

    Create PopularItemDto

---
+++ src/main/java/com/service/acmedeliveryfinal/transfer/PopularItemDto.java
@@ -0,0 +1,11 @@
+package com.service.acmedeliveryfinal.transfer;
+
+import lombok.Value;
+
+@Value
+public class PopularItemDto {
+    Long storeId;
+    Long storeItemId;
+    String storeItemName;
+    Integer timesOrdered;
+}
