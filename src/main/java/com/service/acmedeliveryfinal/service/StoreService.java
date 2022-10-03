1170ca71a92e6e192acd7c5ff56cea497bb66128
g.chatzidakis <g.chatzidakis@pmmretail.com>
2022-10-03 16:19:41 +0300

    Remove unused methods from StoreService

--- src/main/java/com/service/acmedeliveryfinal/service/StoreService.java
+++ src/main/java/com/service/acmedeliveryfinal/service/StoreService.java
@@ -13,50 +13,28 @@ import java.util.Set;
 public interface StoreService extends BaseService<Store>{
     Store getLazy(Long id);
 
-    //List<Store> getLazyAll();
-
-    //void addItems(Store store, Set<StoreItem> items);
-
     void addItem(Long id, StoreItem item);
 
-    /*
-    void removeItem(Long id, Long sid);
 
-    void updateItem(Long id, StoreItem item);
+    List<StoreDetailsDto> getStoreDetailsDtos();
 
+    StoreDto getStoreDto(Long id);
 
-     */
     List<StoreCategory> getAllStoreCategories();
-
-
-    List<KeyValue<Long,String>> getStoresDropdownList(String searchString);
 
     List<Store> getStoresByCategoryId(Long id);
 
-    //List<StoreItem> getProductsByStore(Long id);
-
-    StoreItem getProduct(Long storeId,Long id);
-
-    StoreItem getProduct(Long id);
-
     List<KeyValue<Long,String>> findPopularStores();
-
-    //alternative approach returns a map
-    Map<Long,String> findPopularStoresMap();
-
-    List<KeyValue<Long,String>> findPopularStoresByCategory(String categoryName);
 
     List<KeyValue<Long,String>> findPopularStoresByCategory(Long categoryId);
 
     List<PopularItemDto> findPopularProducts();
 
-    List<KeyValue<Long,String>> findPopularProductsByStore(Long id);
+    List<KeyValue<Long,String>> getStoresDropdownList(String searchString);
 
-    List<StoreDetailsDto> getStoreDetailsDtos();
+    StoreItem getProduct(Long storeId,Long id);
 
-    //StoreDetailsDto getStoreDetailsDto(Long id);
-
-    StoreDto getStoreDto(Long id);
+    StoreItem getProduct(Long id);
 
 
 
