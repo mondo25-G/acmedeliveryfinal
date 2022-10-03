127d99f1b70056f214906cbdf07917b0445b0600
Giorgos <giorgosthf@gmail.com>
2022-10-03 06:03:39 +0300

    Update response cookie config

--- src/main/java/com/service/acmedeliveryfinal/login/jwt/JwtUtils.java
+++ src/main/java/com/service/acmedeliveryfinal/login/jwt/JwtUtils.java
@@ -41,13 +41,11 @@ public class JwtUtils extends BaseComponent {
 
     public ResponseCookie generateJwtCookie(UserDetailsImpl userPrincipal) {
         String jwt = generateTokenFromUsername(userPrincipal.getUsername());
-        ResponseCookie cookie = ResponseCookie.from(jwtCookie, jwt).path("/").maxAge(24 * 60 * 60).httpOnly(true).build();
-        return cookie;
+        return ResponseCookie.from(jwtCookie, jwt).path("/").maxAge(24 * 60 * 60).httpOnly(true).build();
     }
 
     public ResponseCookie getCleanJwtCookie() {
-        ResponseCookie cookie = ResponseCookie.from(jwtCookie, null).path("/").build();
-        return cookie;
+        return ResponseCookie.from(jwtCookie, null).path("/").build();
     }
 
     public String getUserNameFromJwtToken(String token) {
