730c792475922056c821e158a78b90fe60a7eb9b
g.chatzidakis <g.chatzidakis@pmmretail.com>
2022-10-03 16:12:43 +0300

    Refactor AccountService
    remove login method
    refactor register method

--- src/main/java/com/service/acmedeliveryfinal/service/AccountService.java
+++ src/main/java/com/service/acmedeliveryfinal/service/AccountService.java
@@ -7,8 +7,7 @@ import com.service.acmedeliveryfinal.transfer.AccountDto;
 
 public interface AccountService extends BaseService<Account> {
 
-    AccountDto register(String email,String phoneNumber,String password, String firstName,String lastName,String address);
-    AccountDto login(String email, String password);
+    AccountDto login(Long Id);
 
     boolean existsByUsername(String email);
 
